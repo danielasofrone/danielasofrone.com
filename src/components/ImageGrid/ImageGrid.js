9fe90db1530e2550f11ddeb2fc7e34c8c0ea9755
Daniela <daniela.sofrone.sd@gmail.com>
2020-11-29 10:33:29 +0100

    created css image grid

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,49 +1,46 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import images from './images.json'
-import * as S from '../Introduction/introduction.styled'
+// import images from './images.json'
+import * as S from './imageGrid.styled'
 import { withNamespaces } from 'react-i18next';
 
-const useStyles = makeStyles(() => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: "#000",
-  },
-  gridList: {
-    width: "1200px",
-  },
-}));
 
 const ImageGridList = ({t}) => {
 
-  const classes = useStyles();
-
   return (
-    <S.Wrapper isNarrow>
+    <>
+    <S.Wrapper>
      <S.SectionTitle isSmall>
-     <a id="visuals">{t('visuals')}</a> 
+     <a id="visuals">{t('visuals')}</a>
       </S.SectionTitle>
       <S.Subtitle>
         <p>{t('visuals.text.one')}</p>
      <a href="https://www.flickr.com/photos/steepwater" target="blank">  {t('visuals.text.two')}</a>
       </S.Subtitle>
-      <a href="https://www.flickr.com/photos/steepwater" target="blank">
-      <div className={classes.root}>
-      <GridList cellHeight={160} className={classes.gridList} cols={3}>
-        {images.map((image) => (
-          <GridListTile key={image.img} cols={image.cols || 1}>
-            <img src={image.img} alt={image.title} />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
-      </a>
     </S.Wrapper>
+      <S.ImageGridContainer>
+        <S.ImageGridColumn>
+          <img src="https://live.staticflickr.com/65535/50066255662_aea10a8144_h.jpg" />
+          <img src="https://live.staticflickr.com/65535/48560634911_d3a01a6cf9_h.jpg"/>
+          <img src="https://live.staticflickr.com/7814/46589423565_7028356598_h.jpg"/>
+          <img src="https://live.staticflickr.com/65535/48881738212_4badac465c_h.jpg"/>
+          <img src="https://live.staticflickr.com/7921/47438134842_719db2c78d_h.jpg"/>
+        </S.ImageGridColumn>
+        <S.ImageGridColumn >
+          <img src="https://live.staticflickr.com/65535/47729673622_e8d1c785ee_h.jpg"/>
+          <img src="https://live.staticflickr.com/65535/47669669411_9aa4c98f93_h.jpg"/>
+          <img src="https://live.staticflickr.com/3680/33401663286_1530d8b00d_h.jpg"/>
+          <img src= "https://live.staticflickr.com/65535/50045504998_ddd3e06964_h.jpg"/>
+          <img src= "https://live.staticflickr.com/1904/45538926344_678ab9e89e_h.jpg" />
+        </S.ImageGridColumn>
+        <S.ImageGridColumn >
+          <img src="https://live.staticflickr.com/890/26738587907_00e5c2aae8_h.jpg"  />
+          <img src="https://live.staticflickr.com/876/42907150421_92f88cadf9_h.jpg"/>
+          <img src="https://live.staticflickr.com/4161/34321420422_3b79a9c5c3_h.jpg"/>
+          <img src= "https://live.staticflickr.com/897/40432547454_a807afaf87_h.jpg"/>
+          <img src="https://live.staticflickr.com/65535/48122451242_8c87c44f86_h.jpg"/>
+        </S.ImageGridColumn>
+      </S.ImageGridContainer>
+    </>
   );
 }
 
