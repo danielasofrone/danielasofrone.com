f69ceb7d3ca699391f516e656bfda75e1b8d9b9e
Daniela <daniela.sofrone.sd@gmail.com>
2023-05-11 19:16:48 +0200

    updated self portrait

--- src/components/ImageGrid/images.js
+++ src/components/ImageGrid/images.js
@@ -3,7 +3,7 @@ export const images = [
   'https://live.staticflickr.com/4161/34321420422_3b79a9c5c3_h.jpg',
   'https://live.staticflickr.com/65535/52438986499_4c1b6f3017_k.jpg',
   'https://live.staticflickr.com/65535/52791582408_13cf14aa83_n.jpg',
-  'https://live.staticflickr.com/65535/52791078591_00c391ca30_n.jpg',
+  'https://live.staticflickr.com/65535/52889513701_8d28d2fe21_h.jpg"',
   'https://live.staticflickr.com/65535/52791517235_608bdc773b_n.jpg',
   'https://live.staticflickr.com/65535/50045504998_ddd3e06964_h.jpg',
   'https://live.staticflickr.com/65535/52096075655_efa1b6ffe5_k.jpg',
